4b94646193d7d5a8429828c540a95e3a310d6f1d
averybrubeck <averybrubeck@Yahoo.com>
2023-11-17 12:03:35 -0500

    added player class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,11 +1,21 @@
 #class for human vs human game
-class HumanGame
-  attr_accessor :score, :turn, :player1, :player2
+class Player
+  attr_accessor :score, :turn, :player_number
 
-  def initialize(p1, p2, s, t)
-    @player1 = p1
-    @player2 = p2
-    @score = s
-    @turn = t
+  def initialize(player_number, score, turn_number)
+    @player_number = player_number
+    @score = score
+    @turn_number = turn
+  end
+
+  def add_score
+    @score += 1
   end
 end
+
+player1 = Player.new("player1", 0, 0)
+player2 = Player.new("player2", 0, 0)
+puts player1.player_number
+puts player2.player_number
+player1.add_score
+puts player1.score

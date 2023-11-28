cc96c68cd94dc3401dd3df3b2637881c15f56422
averybrubeck <averybrubeck@yahoo.com>
2023-11-27 21:53:33 -0500

    created clear board function

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,15 +1,15 @@
 # tic tac toe board with display, clear and update functions
+
 class Board
   def initialize(player_id)
     @player_id = player_id
   end
 
-  def display_board(board)
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  def clear_board
+    board = ['?', '?', '?', '?', '?', '?', '?', '?', '?']
+    puts board.each_slice(3).map { |row| row.join(" | ") }.join("\n" + "-"*11 + "\n")
   end
 end
 
-board = Board.new(1)
-board.display_board(0)
+new_board = Board.new(1)
+new_board.clear_board

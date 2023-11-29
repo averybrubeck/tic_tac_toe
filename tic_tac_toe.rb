b614e3dd11170aa738464592bda5db895b6bfe02
averybrubeck <averybrubeck@yahoo.com>
2023-11-28 21:08:41 -0500

    added update draw and clear board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,13 +2,32 @@
 class Board
   def initialize(player_id)
     @player_id = player_id
+    @board = ['?', '?', '?', '?', '?', '?', '?', '?', '?']
+  end
+
+  def draw_board
+    puts @board.each_slice(3).map { |row| row.join(" | ") }.join("\n" + "-"*10 + "\n")
   end
 
   def clear_board
-    board = ['?', '?', '?', '?', '?', '?', '?', '?', '?']
-    puts board.each_slice(3).map { |row| row.join(" | ") }.join("\n" + "-"*10 + "\n")
+    @board = ['?', '?', '?', '?', '?', '?', '?', '?', '?']
+    draw_board
+  end
+
+  def update_board_x(position)
+    position = position.to_i
+    @board[position] = 'x'
+    draw_board
+  end
+
+  def update_board_o(position)
+    position = position.to_i
+    @board[position] = 'o'
+    draw_board
   end
 end
 
 new_board = Board.new(1)
 new_board.clear_board
+new_board.update_board_x(1)
+new_board.update_board_o(0)

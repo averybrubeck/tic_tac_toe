32421127efa668f91b166c3703fb1c9531574aa3
averybrubeck <averybrubeck@yahoo.com>
2023-12-02 11:12:48 -0500

    basic game is now working

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,5 +1,7 @@
 # tic tac toe board with display, clear and update functions
 class Board
+  attr_reader :board
+  
   def initialize
     @board = ['?', '?', '?', '?', '?', '?', '?', '?', '?']
     puts "Player 1's Turn"
@@ -13,54 +15,62 @@ class Board
     @board = ['?', '?', '?', '?', '?', '?', '?', '?', '?']
     draw_board
   end
+
+  def update_board(position, marker)
+    @board[position] = marker
+  end
 end
 
 # player class with id
 class Player
-  def initialize(player_id, marker)
+  attr_reader :player_id
+
+  def initialize(player_id, marker, board)
     @player_id = player_id
     @marker = marker.to_s
+    @board = board
     puts 'Invalid Input Try Again' if player_id.length > 1
   end
 
-  def update_board(position, marker)
+  def update_board(position)
     position = position.to_i
-    @board[position] = marker
-    draw_board
+    @board.update_board(position, @marker)
+    @board.draw_board
   end
 end
 
 # Game class storing the logic
 class Game
   def initialize
-    @board = Board.clear_board
-    @current_player = Player.player_id
+    @board = Board.new
+    @player = Player.new('X', 'X', @board)
   end
 
   def play
-    loop do
+      loop do
       @board.draw_board
-      puts "Player #{current_player}, enter your move (0-8)"
+      puts "Player #{@player.player_id}, enter your move (0-8)"
       position = gets.chomp.to_i
 
       if valid_move?(position)
-        @board.update_board_o(position, marker)
+        @player.update_board(position)
         break if game_over?
 
-        @current_player = (current_player == 'X' ? 'O' : 'X')
+        @player = (@player.player_id == 'X' ? Player.new('O', 'O', @board) : Player.new('X', 'X', @board))
       else
         puts 'Invalid Move, Try again'
       end
     end
   end
-end
-
 private
 
 def valid_move?(position)
   position = position.to_i
-  true unless position.negative? || position > 8
+  !(position.negative? || position > 8 || @board.board[position] != '?')
 end
 
 def game_over?
 end
+
+game = Game.new
+game.play end
